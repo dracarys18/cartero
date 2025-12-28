f0b8110cab619b2d35aecfbc842656a64b8fa17a
dracarys18 <me@karthihegde.dev>
2025-12-28 17:12:50 +0530

    feat: do not trim

--- internal/utils/parse_html.go
+++ internal/utils/parse_html.go
@@ -58,8 +58,5 @@ func GetArticleText(u string) (string, error) {
 		return "", fmt.Errorf("failed to extract content: %v", err)
 	}
 
-	if len(article.TextContent) > 4000 {
-		article.TextContent = article.TextContent[:4000] + "..."
-	}
 	return article.TextContent, nil
 }
