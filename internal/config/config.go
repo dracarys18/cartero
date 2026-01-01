e528b5a8593b5d8070e9d56d46e83155eb8daa5a
dracarys18 <me@karthihegde.dev>
2026-01-02 00:50:57 +0530

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -100,8 +100,7 @@ type ProcessorSettings struct {
 	ExtractFieldsSettings
 	TemplateSettings
 	ContentDedupeSettings
-
-	ExtractTextSettings `toml:"extract_text"`
+	ExtractTextSettings
 }
 
 type DedupeSettings struct {
